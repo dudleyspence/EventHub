8bcbf94e9dc4fa24552232112816aec4acb70db1
Dudley Spence <dudleyspence5@gmail.com>
2025-01-17 02:02:53 +0100

    basic middleware set up

--- actions/signup.ts
+++ actions/signup.ts
@@ -5,6 +5,7 @@ import bcrypt from "bcrypt";
 import { db } from "@/lib/db";
 
 import { SignupSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 export async function signup(values: z.infer<typeof SignupSchema>) {
   const validatedFields = SignupSchema.safeParse(values);
@@ -17,11 +18,7 @@ export async function signup(values: z.infer<typeof SignupSchema>) {
 
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  const existingUser = await db.user.findUnique({
-    where: {
-      email,
-    },
-  });
+  const existingUser = await getUserByEmail(email);
 
   if (existingUser) {
     return { error: "This email address is already in use" };
