83777d11fd099e4b70c4e2581881343c4f9f2634
Dudley Spence <dudleyspence5@gmail.com>
2025-02-01 23:00:25 +0100

    lots of progress on the form

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -3,13 +3,17 @@ import React from "react";
 import { NextUIProvider } from "@nextui-org/react";
 import { SessionProvider } from "next-auth/react";
 import { LoginModalProvider } from "@/context/LoginModelProvider";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <LoginModalProvider>
-      <SessionProvider>
-        <NextUIProvider>{children}</NextUIProvider>{" "}
-      </SessionProvider>
-    </LoginModalProvider>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <LoginModalProvider>
+        <SessionProvider>
+          <NextUIProvider>{children}</NextUIProvider>{" "}
+        </SessionProvider>
+      </LoginModalProvider>
+    </LocalizationProvider>
   );
 }
--- components/admin/createEvent/CreateEventForm.tsx
+++ components/admin/createEvent/CreateEventForm.tsx
@@ -1,37 +1,54 @@
 "use client";
 import {
   Button,
-  DatePicker,
+  Checkbox,
   Form,
   Input,
   Select,
   SelectItem,
   Textarea,
 } from "@heroui/react";
-import React from "react";
+import React, { startTransition, useState, useTransition } from "react";
 import { CreateEventSchema } from "@/schemas/event";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Controller, useForm } from "react-hook-form";
 import { z } from "zod";
 import { useCategories } from "@/hooks/useCategories";
-import { now, getLocalTimeZone } from "@internationalized/date";
+import {
+  DesktopDateTimePicker,
+  MobileDateTimePicker,
+} from "@mui/x-date-pickers";
+
+type CreateEventFormInput = z.infer<typeof CreateEventSchema>;
 
 export default function CreateEventForm() {
   const { isLoading, categories } = useCategories();
-  const { handleSubmit, control } = useForm<z.infer<typeof CreateEventSchema>>({
+  const [hasMaxCapacity, setHasMaxCapacity] = useState(false);
+  const [isPending, startTransition] = useTransition();
+  const { handleSubmit, control } = useForm<CreateEventFormInput>({
     resolver: zodResolver(CreateEventSchema),
     defaultValues: {
       title: "",
       description: "",
       category: "",
-      maxCapacity: undefined,
+      maxCapacity: 1,
       image: "",
-      date: undefined,
+      date: new Date(),
     },
   });
 
+  const getToday = () => {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+    return today;
+  };
+
   const onSubmit = (data: z.infer<typeof CreateEventSchema>) => {
-    console.log(data);
+    if (!hasMaxCapacity) {
+      data.maxCapacity = undefined;
+    }
+
+    startTransition(async () => {});
   };
 
   if (isLoading) {
@@ -41,7 +58,7 @@ export default function CreateEventForm() {
   return (
     <Form
       onSubmit={handleSubmit(onSubmit)}
-      className="w-full flex flex-col items-center min-h-[600px] mb-10 my-5 p-10"
+      className="w-full flex flex-col items-center min-h-[700px] mb-10 my-5 p-10"
     >
       <Controller
         control={control}
@@ -53,57 +70,91 @@ export default function CreateEventForm() {
             {...field}
             label="Event Name"
             variant="bordered"
-            type="text"
             isInvalid={fieldState.invalid}
             errorMessage={fieldState.error?.message}
           />
         )}
       />
       <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-5 pt-5">
-        <Controller
-          control={control}
-          name="category"
-          render={({ field }) => (
-            <Select className="w-full" label="Category" {...field}>
-              {categories.map((category) => (
-                <SelectItem key={category.name} value={category.name}>
-                  {category.name}
-                </SelectItem>
-              ))}
-            </Select>
-          )}
-        />
-        <Controller
-          control={control}
-          name="maxCapacity"
-          render={({ field, fieldState }) => (
-            <Input
-              {...field}
-              label="Maximum Capacity"
-              variant="bordered"
-              type="number"
-              isInvalid={fieldState.invalid}
-              errorMessage={fieldState.error?.message}
-              onChange={(e) => field.onChange(Number(e.target.value))}
-            />
-          )}
-        />
-        <div className="w-full flex flex-row gap-4">
+        <div className="w-full flex flex-col gap-7">
+          <Controller
+            control={control}
+            name="category"
+            render={({ field }) => (
+              <Select className="w-full" label="Category" {...field}>
+                {categories.map((category) => (
+                  <SelectItem key={category.name} value={category.name}>
+                    {category.name}
+                  </SelectItem>
+                ))}
+              </Select>
+            )}
+          />
           <Controller
             control={control}
             name="date"
-            render={({ field }) => (
-              <DatePicker
-                {...field}
-                hideTimeZone
-                showMonthAndYearPickers
-                defaultValue={new Date()}
-                label="Event Date"
-                variant="bordered"
-              />
+            render={({ field, fieldState }) => (
+              <div className="w-full">
+                <div className="md:hidden block w-full">
+                  <MobileDateTimePicker
+                    className="w-full shadow-sm"
+                    {...field}
+                    label="Event Date"
+                    minDateTime={new Date()}
+                    disablePast
+                    value={field.value ?? null}
+                    onChange={(date) => field.onChange(date ?? undefined)}
+                  />
+                </div>
+                <div className="hidden md:block w-full">
+                  <DesktopDateTimePicker
+                    {...field}
+                    className="w-full shadow-sm"
+                    slotProps={{ textField: { sx: { borderRadius: "8px" } } }}
+                    label="Event Date"
+                    value={field.value}
+                    disablePast
+                    minDate={getToday()}
+                    onChange={(newValue) => field.onChange(newValue)}
+                  />
+                </div>
+              </div>
             )}
           />
+          <div>
+            <Checkbox
+              className="mb-2"
+              checked={hasMaxCapacity}
+              onChange={() => {
+                setHasMaxCapacity(!hasMaxCapacity);
+              }}
+            >
+              Does this event have a max capacity?
+            </Checkbox>
+
+            <Controller
+              control={control}
+              name="maxCapacity"
+              render={({ field, fieldState }) => (
+                <Input
+                  {...field}
+                  value={field.value !== undefined ? String(field.value) : ""}
+                  onChange={(e) => {
+                    const value = e.target.value;
+                    field.onChange(value === "" ? undefined : Number(value));
+                  }}
+                  isDisabled={!hasMaxCapacity}
+                  label="Maximum Capacity"
+                  variant="bordered"
+                  type="number"
+                  isInvalid={fieldState.invalid}
+                  errorMessage={fieldState.error?.message}
+                />
+              )}
+            />
+          </div>
         </div>
+
         <Controller
           control={control}
           name="image"
