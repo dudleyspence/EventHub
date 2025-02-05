a4a50c0a4e6837aae8f6ed319f7f9cd32095e799
Dudley Spence <dudleyspence5@gmail.com>
2025-02-06 00:07:08 +0100

    updates categories to have a description

--- app/(non-auth)/(public)/events/category/[category]/page.tsx
+++ app/(non-auth)/(public)/events/category/[category]/page.tsx
@@ -1,5 +1,7 @@
 import EventsListContainer from "@/components/eventslist/EventsListContainer";
 import { fetchCategories } from "@/lib/actions/fetchCategories";
+import { fetchCategory } from "@/lib/actions/fetchCategory";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 import React from "react";
 
@@ -10,21 +12,32 @@ interface PageProps {
 }
 
 export default async function page({ params }: PageProps) {
-  let { category } = await params;
+  const { category } = await params;
 
-  const categoryList = await fetchCategories();
-  category = category ? decodeURIComponent(category) : undefined;
+  const decodedCategory = category ? decodeURIComponent(category) : undefined;
 
-  if (category) {
-    category = categoryList.includes(category) ? category : undefined;
-  }
+  const fullCategory = decodedCategory
+    ? await fetchCategory(decodedCategory)
+    : undefined;
 
   return (
     <div className="pt-10 w-full">
-      <h1 className="w-full text-center font-bold text-5xl mb-10">
-        Browse Events
-      </h1>
-      <EventsListContainer category={category} />;
+      {fullCategory && (
+        <div className="w-full bg-yellow-100 h-[280px] mb-5 flex flex-col-reverse sm:flex-row justify-between rounded-md">
+          <h1 className="text-left pl-10 p-5 sm:p-10  font-bold text-5xl mb-10">
+            {fullCategory.name}
+          </h1>
+          <div className="relative sm:h-[280px] w-full h-[200px] sm:w-1/2 ">
+            <Image
+              fill
+              className="object-cover rounded-md"
+              src={fullCategory.image}
+              alt={fullCategory.name}
+            />
+          </div>
+        </div>
+      )}
+      <EventsListContainer category={decodedCategory} />;
     </div>
   );
 }
--- app/(non-auth)/(public)/events/page.tsx
+++ app/(non-auth)/(public)/events/page.tsx
@@ -1,12 +1,15 @@
 import EventsListContainer from "@/components/eventslist/EventsListContainer";
+import Image from "next/image";
 import React from "react";
 
 export default function page() {
   return (
     <div className="pt-10 w-full">
-      <h1 className="w-full text-center font-bold text-5xl mb-10">
-        Browse Events
-      </h1>
+      <div className="w-full bg-yellow-100 h-[250px] mb-5">
+        <h1 className="w-full text-left p-10 font-bold text-5xl mb-10">
+          Browse Events
+        </h1>
+      </div>
       <EventsListContainer />;
     </div>
   );
