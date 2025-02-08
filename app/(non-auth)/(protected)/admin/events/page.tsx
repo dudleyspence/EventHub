942d86e57b147ddf523be2fed4d278116a26b5ff
Dudley Spence <dudleyspence5@gmail.com>
2025-02-08 11:32:05 +0100

    admin categories updated

--- app/(non-auth)/(protected)/admin/events/page.tsx
+++ app/(non-auth)/(protected)/admin/events/page.tsx
@@ -1,13 +1,16 @@
 import EventsListContainer from "@/components/eventslist/EventsListContainer";
+import { fetchCategories } from "@/lib/actions/fetchCategories";
 import React from "react";
 
-export default function page() {
+export default async function page() {
+  const allCategories = await fetchCategories();
+
   return (
     <div className="pt-10 w-full">
       <h1 className="w-full text-center font-bold text-5xl mb-10">
         Browse Events
       </h1>
-      <EventsListContainer />;
+      <EventsListContainer categories={allCategories} />;
     </div>
   );
 }
