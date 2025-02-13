6f196aa25f60d7114c2c9df726c225ead8f9fca2
Dudley Spence <dudleyspence5@gmail.com>
2025-02-13 22:01:58 +0100

    events lazy loading

--- components/eventslist/EventsList.tsx
+++ components/eventslist/EventsList.tsx
@@ -1,8 +1,14 @@
-"use client";
 import React from "react";
 import EventCard from "../EventCard";
 import { EventListEvent } from "@/types/events";
 import Image from "next/image";
+import dynamic from "next/dynamic";
+import EventCardSkeleton from "../loading/EventCardSkeleton";
+
+const LazyEventCard = dynamic(() => import("../EventCard"), {
+  ssr: true,
+  loading: () => <EventCardSkeleton />,
+});
 
 interface EventListProps {
   events: EventListEvent[];
@@ -24,17 +30,29 @@ export default function EventList({ events }: EventListProps) {
 
   return (
     <div className="w-full grid sm:grid-cols-2 place-items-center gap-5 mt-8 px-5">
-      {events.map((event) => (
-        <EventCard
-          key={event.id}
-          id={event.id}
-          name={event.title}
-          date={event.date}
-          totalAttendees={event.totalAttendees}
-          image={event.image}
-          category={event.category}
-        />
-      ))}
+      {events.map((event, index) =>
+        index < 2 ? (
+          <EventCard
+            key={"event_" + event.id}
+            id={event.id}
+            name={event.title}
+            date={event.date}
+            totalAttendees={event.totalAttendees}
+            image={event.image}
+            category={event.category}
+          />
+        ) : (
+          <LazyEventCard
+            key={event.id + "_LazyEventCard"}
+            id={event.id}
+            name={event.title}
+            date={event.date}
+            totalAttendees={event.totalAttendees}
+            image={event.image}
+            category={event.category}
+          />
+        )
+      )}
     </div>
   );
 }
