1a544668b1596755554b885c7b86696cc9b28133
Dudley Spence <dudleyspence5@gmail.com>
2025-02-14 17:40:38 +0100

    quick update on loading screen for eventList pages - no longer needs to inclide banner and filters

--- app/(non-auth)/(public)/events/category/[category]/loading.tsx
+++ app/(non-auth)/(public)/events/category/[category]/loading.tsx
@@ -1,21 +1,6 @@
-import FiltersSkeleton from "@/components/loading/FiltersSkeleton";
 import LoadingList from "@/components/loading/LoadingList";
 import React from "react";
 
 export default function loading() {
-  return (
-    <div className="pt-10 w-full">
-      <div className="w-full bg-yellow-100 flex flex-row justify-start h-[400px] sm:h-[280px] p-10 mb-5"></div>
-      <div className="w-full flex flex-col items-center gap-16">
-        <div className="w-full flex flex-row gap-5">
-          <div id="filters" className="hidden lg:block lg:w-1/4">
-            <FiltersSkeleton />
-          </div>
-          <div id="event-list" className="w-full lg:w-3/4">
-            <LoadingList eventsPerPage={10} />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <LoadingList eventsPerPage={10} />;
 }
